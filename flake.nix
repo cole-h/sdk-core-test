504d86848f8ed55b512495b706d0bb00bcac4f94
Cole Helbling <cole.e.helbling@outlook.com>
2023-11-20 07:43:00 -0800

    successful sdk-core build

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "aarch64-darwin" "x86_64-darwin" "x86_64-linux" "x86_64-linux" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+
+      packages = forEachSupportedSystem ({ pkgs }: rec {
+        default = test;
+
+        test = pkgs.rustPlatform.buildRustPackage {
+            name = "sdk-core-test";
+            src = self;
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+              outputHashes = {
+                "rustfsm-0.1.0" = "sha256-Z7LHN6AF+bJ5Poa7cIqGQlPpX8YBmOpnDX+QqtcyRus=";
+                "temporal-sdk-core-protos-0.1.0" = "sha256-zJTinh/a0baEvLL/DZlyGwiYUk9VIEZmZrQsKXn6n1E=";
+              };
+            };
+            nativeBuildInputs = with pkgs; [ protobuf ];
+          };
+      });
+
+    };
+}
+
